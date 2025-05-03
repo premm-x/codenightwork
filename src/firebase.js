ad2d8c113418bcc3fb718185c2f2c5070a9c2767
prem sharma <mybrother1370@gmail.com>
2025-05-03 18:10:58 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvfZY-pYKubTF8u55ztRNvNdFhiyqwA7g",
-  authDomain: "collegecampus-e2017.firebaseapp.com",
-  projectId: "collegecampus-e2017",
-  storageBucket: "collegecampus-e2017.firebasestorage.app",
-  messagingSenderId: "974664696052",
-  appId: "1:974664696052:web:add1a4f442b396d81b9ea1",
-  measurementId: "G-XWKQ1MN7KL"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
